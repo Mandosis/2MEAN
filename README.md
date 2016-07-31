c5b0d5f86bd9e43ae90c001a60720c801501e993
Chris Rabuse <rab233@live.com>
2016-07-31 16:33:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Angular 2 MEAN Stack Starter
+2MEAN is a starter for working with Angular 2, Angular 2 Universal, TypeScript and the MEAN Stack.
---
+++ src/server/server.ts
@@ -0,0 +1,39 @@
+// the polyfills must be the first thing imported in node.js
+import 'angular2-universal/polyfills';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import * as path from 'path';
+
+// Angular 2
+import { enableProdMode } from '@angular/core';
+// Angular 2 Universal
+import { expressEngine } from 'angular2-universal';
+
+// enable prod for faster renders
+enableProdMode();
+
+const app = express();
+const ROOT = path.join(path.resolve(__dirname, '..'));
+
+// Express View
+app.engine('.html', expressEngine);
+app.set('views', __dirname);
+app.set('view engine', 'html');
+
+app.use(cookieParser('Angular 2 Universal'));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(ROOT, 'dist/client'), {index: false}));
+
+import { ngApp } from './routes/ngApp';
+
+app.get('/', ngApp);
+app.get('/home', ngApp);
+app.get('/home/*', ngApp);
+
+
+
+let server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on: http://localhost:${server.address().port}`);
+});
